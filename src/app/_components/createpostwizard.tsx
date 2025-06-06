8097bfaf8f4ee1d945670bf7670cdd46634c5271
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-06-06 23:03:45 +0530

    changed some stuff

--- src/app/_components/createpostwizard.tsx
+++ src/app/_components/createpostwizard.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function CreatePostWizard() {
   const { user } = useUser();
@@ -10,10 +11,12 @@ export default function CreatePostWizard() {
 
   return (
     <div className="flex w-full gap-3 border-b border-slate-400 p-8">
-      <img
+      <Image
         className="h-14 w-14 rounded-full"
         src={user.imageUrl}
         alt="Profile Picture"
+        width={56}
+        height={56}
       />
       <input
         type="text"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import { api } from "~/trpc/server";
+import Image from "next/image";
 import CreatePostWizard from "~/app/_components/createpostwizard";
 import type { RouterOutputs } from "~/trpc/react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs";
+
+dayjs.extend(relativeTime);
 
 type PostWithUser = RouterOutputs["post"]["getAll"][number];
 const PostView = (props: PostWithUser) => {
@@ -10,16 +15,18 @@ const PostView = (props: PostWithUser) => {
       key={post.id}
       className="flex items-center gap-3 border-b border-slate-400 p-4"
     >
-      <img
+      <Image
         className="h-12 w-12 rounded-full"
         src={author.imageUrl}
+        width={56}
+        height={56}
         alt="Profile Picture"
       />
       <div className="flex flex-col">
-        <div className="flex">
-          <span className="font-bold text-slate-300">{`@${author.username!}`}</span>
+        <div className="flex items-center font-thin">
+          <span className="font-semibold text-slate-300">{`@${author.username!}`}</span>
           <span className="mx-2 text-slate-300">·</span>
-          <span className="text-slate-300">1 hour ago</span>
+          <span className="text-sm text-slate-300">{`${dayjs(post.createdAt).endOf("D")}`}</span>
         </div>
         <span>{post.content}</span>
       </div>
