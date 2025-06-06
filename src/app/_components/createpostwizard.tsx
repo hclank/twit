060d6379962f79b1cceaf0da5c8bdf8c135dc00e
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-06-06 21:00:09 +0530

    working on ui and added some stuff going to go onto the backend stuff now with tRPC kinda fired up

---
+++ src/app/_components/createpostwizard.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+
+export default function CreatePostWizard() {
+  const { user } = useUser();
+
+  if (!user) return null;
+
+  return (
+    <div className="flex w-full gap-3 border-b border-slate-400 p-8">
+      <img
+        className="h-14 w-14 rounded-full"
+        src={user.imageUrl}
+        alt="Profile Picture"
+      />
+      <input
+        type="text"
+        placeholder="Send some emojis..."
+        className="grow bg-transparent outline-none"
+      />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import { api } from "~/trpc/server";
+import CreatePostWizard from "~/app/_components/createpostwizard";
 
 export default async function Home() {
   const posts = await api.post.getAll();
+
+  if (!posts) return <div>Loading...</div>;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div>{posts?.map((post) => <div key={post.id}>{post.content}</div>)}</div>
+    <main className="flex h-screen justify-center">
+      <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
+        <CreatePostWizard />
+        <div className="flex flex-col">
+          {posts.map((post) => (
+            <div key={post.id} className="border-b border-slate-400 p-8">
+              {post.content}
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
